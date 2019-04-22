07797c9f5de12c42b8bccecac0530f2f83957dcd
Mikael Lirbank <mikael@airlab.io>
2019-04-22 09:36:02 -0700

    Prettier

--- src/queue.js
+++ src/queue.js
@@ -3,18 +3,17 @@ import assert from 'assert';
 
 export default class Queue {
   queue: string;
-  AMQP: {send: Function, registerWorker: Function};
+  AMQP: { send: Function, registerWorker: Function };
   done: Promise<*>; // TODO: annotate type explicitly
-  static bind: Function
+  static bind: Function;
 
   constructor(
-    amqp: {send: Function, registerWorker: Function},
+    amqp: { send: Function, registerWorker: Function },
     queue: string,
-    options: {prefetch: ?number, retryDelay: ?number} | Function,
-    worker: ?Function
+    options: { prefetch: ?number, retryDelay: ?number } | Function,
+    worker: ?Function,
   ) {
-
-    assert(typeof queue === 'string', 'The \'queue\' param is required');
+    assert(typeof queue === 'string', "The 'queue' param is required");
 
     this.queue = queue;
     this.AMQP = amqp;
@@ -32,19 +31,18 @@ export default class Queue {
   }
 
   async registerWorker(
-    options: {prefetch: ?number, retryDelay: ?number} | Function,
-    worker: ?Function
+    options: { prefetch: ?number, retryDelay: ?number } | Function,
+    worker: ?Function,
   ) {
-
     // Make options optional
     if (typeof options === 'function') {
       worker = options;
       options = {
         prefetch: 1,
-        retryDelay: 60000
+        retryDelay: 60000,
       };
     }
-    const {prefetch, retryDelay} = options;
+    const { prefetch, retryDelay } = options;
     await this.AMQP.registerWorker(this.queue, worker, prefetch, retryDelay);
   }
 }
